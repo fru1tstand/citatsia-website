eb89ed78345cbd418ecaf7de44fe206b9b13d750
Kodlee Yin <kodlee@fru1t.me>
2017-03-04 16:54:14 -0800

    added gametracker to home page

--- php/me/fru1t/citatsia/content/home.php
+++ php/me/fru1t/citatsia/content/home.php
@@ -3,8 +3,52 @@ namespace me\fru1t\citasia\content;
 use me\fru1t\citatsia\template\EmptyPage;
 
 $body = <<<HTML
+<div class="spacer content"></div>
+<div class="container centered">
+  <div class="section-title">Servers</div>
+  <div class="spacer section-title"></div>
+  
+  <div class="gametracker-wrapper">
+    <iframe class="gametracker"
+            src="http://cache.gametracker.com/components/html0/?host=162.248.94.109:27016&bgColor=FFFFFF&fontColor=000000&titleBgColor=FFFFFF&titleColor=000000&borderColor=FFFFFF&linkColor=00A001&borderLinkColor=000000&showMap=0&showCurrPlayers=0&showTopPlayers=0&showBlogs=0&width=250"
+            frameborder="0"
+            scrolling="no"></iframe>
+    <a href="steam://connect/162.248.94.109:27016">Join</a>
+  </div>
+  <div class="gametracker-wrapper">
+    <iframe class="gametracker"
+            src="http://cache.gametracker.com/components/html0/?host=66.85.14.117:27015&bgColor=FFFFFF&fontColor=000000&titleBgColor=FFFFFF&titleColor=000000&borderColor=FFFFFF&linkColor=00A001&borderLinkColor=000000&showMap=0&showCurrPlayers=0&showTopPlayers=0&showBlogs=0&width=250"
+            frameborder="0"
+            scrolling="no"></iframe>
+    <a href="steam://connect/66.85.14.117:27015">Join</a>
+  </div>
+  <div class="gametracker-wrapper">
+    <iframe class="gametracker"
+            src="http://cache.gametracker.com/components/html0/?host=66.150.164.139:27015&bgColor=FFFFFF&fontColor=000000&titleBgColor=FFFFFF&titleColor=000000&borderColor=FFFFFF&linkColor=00A001&borderLinkColor=000000&showMap=0&showCurrPlayers=0&showTopPlayers=0&showBlogs=0&width=250"
+            frameborder="0"
+            scrolling="no"></iframe>
+    <a href="steam://connect/66.150.164.139:27015">Join</a>
+  </div>
+  <div class="gametracker-wrapper">
+    <iframe class="gametracker"
+            src="http://cache.gametracker.com/components/html0/?host=208.146.44.122:27015&bgColor=FFFFFF&fontColor=000000&titleBgColor=FFFFFF&titleColor=000000&borderColor=FFFFFF&linkColor=00A001&borderLinkColor=000000&showMap=0&showCurrPlayers=0&showTopPlayers=0&showBlogs=0&width=250"
+            frameborder="0"
+            scrolling="no"></iframe>
+    <a href="steam://connect/208.146.44.122:27015">Join</a>
+  </div>
+</div>
+
+<div class="spacer content"></div>
+<div class="container">
+  <div class="section-title">News</div>
+  <div class="spacer section-title"></div>
+  
+  <div class="forum">
+    
+  </div>
+</div>
+
 Home - Game servers, Latest announcements/news
-<div style="height: 1000px;"></div>
 HTML;
 
 EmptyPage::start()
